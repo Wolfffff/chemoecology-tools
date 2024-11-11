787f7e5c943f312207f4a6538ba1540b1fe1fde4
Wolfffff <22243650+Wolfffff@users.noreply.github.com>
2024-11-10 23:36:57 -0500

    update

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,6 @@ copyright = "2024, Scott Wolf"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_click",
     "myst_parser",
 ]
 autodoc_typehints = "description"
--- docs/usage.md
+++ docs/usage.md
@@ -1,7 +1,3 @@
 # Usage
 
-```{eval-rst}
-.. click:: chemoecology_tools.__main__:main
-    :prog: chemoecology-tools
-    :nested: full
-```
+
